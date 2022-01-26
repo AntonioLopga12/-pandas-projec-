486fa902b612fd2d52213b34c41fc99cddad6ced
AntonioLopga12 <antoniolopgalvear@icloud.com>
2022-01-26 15:51:22 +0100

    update

--- src/clean_functions.py
+++ src/clean_functions.py
@@ -1,6 +1,11 @@
 
-#porcentaje
-def dirt(x):
-    x = x[0]
-    porcentaje = (x*100)/6302
-    return f'The percentage of Nan values in this column is of, {round(porcentaje,2)}%.'
+#porcentaje de valores 'Nan', por columna.
+def dirt(sha):
+
+    dic = dict(sha.isna().sum())
+
+    for col,nan in dic.items():
+        percent = (nan*100)/dic.shape[0]
+
+    print(f'La columna {col} tiene {round(percent*100,2)}% de valores Nan!') 
+                 
